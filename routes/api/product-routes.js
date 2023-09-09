7bfea004ce63c103aa7727a61beb8a42f3fd146e
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-09 13:17:59 -0700

    Update Product route to include tag model

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: {model:Category}
+      include: [{model:Category}, {model:Tag}]
     })
     res.status(200).json(productData);
   }catch (err){
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async(req, res) => {
 try {
   const productData = await Product.findByPk(req.params.id, {
-    include: {model:Category}
+    include: [{model:Category}, {model:Tag}]
   })
   //if ID cant be found ... 
   if (!productData) {
@@ -36,14 +36,6 @@ try {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
