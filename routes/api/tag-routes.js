a3691b6cbaa43ee835617869841f72223e8aa465
Bryan Bickel <briimcfly@Bryans-Mac-mini.local>
2023-09-12 17:28:55 -0700

    updated Tag route.. Still not displaying Products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,18 +48,46 @@ router.post('/', async(req, res) => {
 router.put('/:id', async (req, res) => {
   try {
   const tagData = await Tag.update({
-    tag_name: req.body.tag_name
+    tag_name: req.body.tag_name,
+    productIds: req.body.productIds
   },
   {
     where: {
       id: req.params.id
     }
-  })
+  });
 
   // No ID Found
   if (!tagData) {
     res.status(404).json({message: "No Tag found with that ID"})
   }
+
+  if (req.body.productIds && req.body.productIds.length) { 
+  const productTags = await ProductTag.findAll({ where: { tag_id: req.params.id } 
+  });
+
+  //create filtered list of new product_ids
+  const productTagIds = productTags.map(({product_id})=> product_id);
+  const newProductTags = req.body.productIds
+  .filter((product_id) => !productTagIds.includes(product_id))
+  .map((product_id)=> {
+    return {
+      tag_id: req.params.id,
+      product_id,
+    }
+  });
+
+        //figure out which ones to remove 
+        const productTagsToRemove = productTags
+        .filter(({product_id}) => !req.body.productIds.includes(product_id))
+        .map(({id}) => id);
+        
+        //run both actions
+        return Promise.all([
+          ProductTag.destroy({where: {id: productTagsToRemove}}),
+          ProductTag.bulkCreate(newProductTags),
+        ]) 
+    }
   //success
   res.status(200).json(tagData);
   }catch (err) {
